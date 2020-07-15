4011906f4d17924a7a674dbffacd2e4645610dfd
Kitaeza <kitaevsd@outlook.com>
2020-07-16 00:36:03 +0300

    Update items.py

--- leroymerlin/items.py
+++ leroymerlin/items.py
@@ -4,28 +4,28 @@
 # https://docs.scrapy.org/en/latest/topics/items.html
 
 import scrapy
+from scrapy.loader import ItemLoader
 from scrapy.loader.processors import MapCompose, TakeFirst
 
-def specification_gen(keys, values):
+def price_mod(value):
     if not value: return []
-    print(1)
-    # item['price'] = item['price'].replace(' ', '')
-    # item['spec_vals'] = [key.replace('\n', '').strip() for key in item['spec_vals']]
-    # item['specifications'] = dict(zip(item['spec_keys'], item['spec_vals']))
-    # del item['spec_keys']
-    # del item['spec_vals']
+    return value.replace(' ', '')
+
+def specification_value_mod(value):
+    if not value: return []
+    return value.strip()
+
 
 class LeroymerlinItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
     _id = scrapy.Field()
-    href = scrapy.Field()
-    title = scrapy.Field()
-    price = scrapy.Field()
-    currency = scrapy.Field()
-    unit = scrapy.Field()
+    href = scrapy.Field(output_processor=TakeFirst())
+    title = scrapy.Field(output_processor=TakeFirst())
+    price = scrapy.Field(input_processor=MapCompose(price_mod), output_processor=TakeFirst())
+    currency = scrapy.Field(output_processor=TakeFirst())
+    unit = scrapy.Field(output_processor=TakeFirst())
     spec_keys = scrapy.Field()
-    spec_vals = scrapy.Field()
-    specifications = scrapy.Field(input_processor=MapCompose(specification_gen))
+    spec_vals = scrapy.Field(input_processor=MapCompose(specification_value_mod))
+    specifications = scrapy.Field()
     photos = scrapy.Field()
-    pass
