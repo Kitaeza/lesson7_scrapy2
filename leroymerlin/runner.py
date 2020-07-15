71217a3653b39904a7e83151956a0c2e7b1d1cc8
Kitaeza <kitaevsd@outlook.com>
2020-07-16 00:33:33 +0300

    Update runner.py

--- leroymerlin/runner.py
+++ leroymerlin/runner.py
@@ -4,13 +4,19 @@ from scrapy.crawler import CrawlerProcess
 from leroymerlin import settings
 from leroymerlin.spiders.lmru import LmruSpider
 
+
 if __name__ == '__main__':
-    unit = 'бассейн'
+    unit = 'унитаз'
+
+    photo_settings = settings
+    photo_settings.IMAGES_STORE = unit
 
     crowler_settings = Settings()
-    crowler_settings.setmodule(settings)
+    crowler_settings.setmodule(photo_settings)
 
     process = CrawlerProcess(settings=crowler_settings)
     process.crawl(LmruSpider, unit)
 
     process.start()
+
+
